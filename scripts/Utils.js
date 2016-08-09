a74cb830147d78f50838c1f705513cc3da43b810
Bruce McPherson <bruce@mcpher.com>
2016-08-09 05:03:42 -0700

    updated by GasGit automation

--- scripts/Utils.js
+++ scripts/Utils.js
@@ -373,5 +373,25 @@ var Utils = (function (ns) {
     return typeof value === "object" && !Array.isArray(value);
   }
   
+  /**
+   * crush for writing to cache.props
+   * @param {string} crushThis the string to crush
+   * @return {string} the b64 zipped version
+   */
+  ns.crush = function (crushThis) {
+    return Utilities.base64Encode(Utilities.zip ([Utilities.newBlob(JSON.stringify(crushThis))]).getBytes());
+  };
+
+  /**
+   * uncrush for writing to cache.props
+   * @param {string} crushed the crushed string
+   * @return {string} the uncrushed string
+   */
+  ns.uncrush = function (crushed) {
+    return Utilities.unzip(Utilities.newBlob(Utilities.base64Decode(crushed),'application/zip'))[0].getDataAsString();
+  };
+
+
+
   return ns;
 }) (Utils || {});
